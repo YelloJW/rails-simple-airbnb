244e2d031ae5efcdcd6ab39bae2c2d3369822e18
Joe Olley <joeolley@hotmail.co.uk>
2019-11-13 17:03:38 +0000

    Flats model controller routes and CRUD added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Terrace house in  London',
+  address: '3 dale lane London W9 1DT',
+  description: 'Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 100,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Homely house London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Rooftop Flat London',
+  address: 'Embankment London W9 1DT',
+  description: 'Fabulous pad overlooking the Thames',
+  price_per_night: 300,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: 'large Flat London',
+  address: 'Somewhere else London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 150,
+  number_of_guests: 6
+)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: 'Somewhere else London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 80,
+  number_of_guests: 7
+)
